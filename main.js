b7d53d2dcf6dd28b9b7cd391570a8ca7083cc332
danwilliams92 <34779248+danwilliams92@users.noreply.github.com>
2018-01-18 10:28:36 +0000

    Add files via upload
    Updated course information with javascript and more css

---
+++ main.js
@@ -0,0 +1,63 @@
+function toggle1(){
+	var ele = document.getElementById("moreDetails1");
+
+	if(ele.style.display == "block") {
+
+    		ele.style.display = "none";
+
+  	}
+
+	else {
+		ele.style.display = "block";
+
+	}
+
+} 
+
+function toggle2(){
+	var ele = document.getElementById("moreDetails2");
+
+	if(ele.style.display == "block") {
+
+    		ele.style.display = "none";
+
+  	}
+
+	else {
+		ele.style.display = "block";
+
+	}
+
+} 
+
+function toggle3(){
+	var ele = document.getElementById("moreDetails3");
+
+	if(ele.style.display == "block") {
+
+    		ele.style.display = "none";
+
+  	}
+
+	else {
+		ele.style.display = "block";
+
+	}
+
+} 
+
+function toggle4(){
+	var ele = document.getElementById("moreDetails4");
+
+	if(ele.style.display == "block") {
+
+    		ele.style.display = "none";
+
+  	}
+
+	else {
+		ele.style.display = "block";
+
+	}
+
+} 
